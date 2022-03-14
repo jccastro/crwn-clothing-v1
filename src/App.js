d47b55bc32cb661659cc28cb06d5113c022602a6
jccastro <jc.castro2636@uniandes.edu.co>
2022-03-13 22:50:22 -0500

    update menu-items to navigate withRouder HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.scss";
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
+  const HatsPage = () => {
+    return (
+      <div>
+        <h1>HATS PAGE</h1>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
