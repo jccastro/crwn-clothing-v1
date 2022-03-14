1f341f517daa465c6ece5167a0c6c76c248e434f
jccastro <jc.castro2636@uniandes.edu.co>
2022-03-14 12:04:51 -0500

    changed shopPage from class component to functional component, included hooks in collection preview component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import "./collection-preview.styles.scss";
+import { useState, useEffect } from "react";
 
 const CollectionPreview = ({ title, items }) => {
+  const [filteredItems, setFilteredItems] = useState([]);
+  useEffect(() => {
+    setFilteredItems(items.filter((_, i) => i < 4));
+  }, [items]);
+
   return (
     <div className="collection-preview">
       <h1 className="title"> {title.toUpperCase()} </h1>
       <div className="preview">
-        {items
-          .filter((_, i) => i < 4)
-          .map(({ id, name }) => {
-            return (
-              <div className="item" key={id}>
-                {name}
-              </div>
-            );
-          })}
+        {filteredItems.map(({ id, name }) => {
+          return (
+            <div className="item" key={id}>
+              {name}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 import SHOP_DATA from "./shop.data";
+import { useState, useEffect } from "react";
 
-class ShopPage extends React.Component {
-  constructor() {
-    super();
-    this.state = { collections: SHOP_DATA };
-  }
+const ShopPage = () => {
+  const [collections, setCollections] = useState([]);
+  useEffect(() => {
+    setCollections(SHOP_DATA);
+  }, []);
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => {
-          return <CollectionPreview key={id} {...otherCollectionProps} />;
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => {
+        return <CollectionPreview key={id} {...otherCollectionProps} />;
+      })}
+    </div>
+  );
+};
+
+// class ShopPage extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = { collections: SHOP_DATA };
+//   }
+
+//   render() {
+//     const { collections } = this.state;
+//     return (
+//       <div className="shop-page">
+//         {collections.map(({ id, ...otherCollectionProps }) => {
+//           return <CollectionPreview key={id} {...otherCollectionProps} />;
+//         })}
+//       </div>
+//     );
+//   }
+// }
 
 export default ShopPage;
